3c66d6c9b9dc3f9964644acf7d23bafc53f4466f
mKoleva <ml95koleva@gmail.com>
2016-10-15 16:00:42 +0100

    first graph now working with real data

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ server.listen(port, function(){
 });
 
 const query = client.query(
-	"select COUNT(id), primary_type from crimes where primary_type = 'HOMICIDE' OR primary_type = 'BURGLARY' OR primary_type = 'ASSAULT' OR primary_type = 'KIDNAPPING' OR primary_type = 'SEX OFFENSE' group by primary_type");
+	"select * from(select COUNT(id), primary_type from crimes where primary_type != 'OTHER OFFENSE' group by primary_type ORDER BY COUNT(id) DESC) limit 5");
 
 query.on('row', (row) => {
 	results.push(row);
@@ -33,17 +33,16 @@ query.on('end', () => {
 		graphData[i] = {
 			"title" : results[i]["primary_type"],
 			"subtitle" : "",
-			"ranges" : [0, results[i]['count']/2, parseInt(results[i]['count'])],
+			"ranges" : [0, results[i]['count']/2, 1300000],
 			"measures" : [parseInt(results[i]['count']), parseInt(results[i]['count'])],
 			"markers" : [parseInt(results[i]['count']) + 100]
 		};
-		
 	}
 });
 
-server.post('/first.html', function(req, res){
+/*server.post('/first.html', function(req, res){
 	return res.redirect('/first.html');
-});
+});*/
 
 server.post('/update_date', function(req,res){
 	
@@ -51,6 +50,7 @@ server.post('/update_date', function(req,res){
 
 server.get('/get_primary_type', function(req,res){
 	res.setHeader('Content-Type', 'application/json');
+	console.log(graphData);
 	res.send(JSON.stringify(graphData));
 });
 
