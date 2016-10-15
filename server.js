f36d4a9ec55087159b3fff72c5b130a246b0df84
mKoleva <ml95koleva@gmail.com>
2016-10-15 12:47:47 +0100

    added datepicker to statistics

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var server = express();
 server.use(express.static(__dirname + '/public'));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.post('/first.html', function(req, res){
+	return res.redirect('/first.html');
+});
 
 var port = 10001;
 server.listen(port, function(){
