09e3ad0c628da42eb09037bf66e9ea4b1a8b5532
mKoleva <ml95koleva@gmail.com>
2016-10-15 19:31:41 +0100

    first chart using highcharts

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var conString = "postgres://vesko:Veskotesco69@hsoc-prod.cwyfzstkekxt.eu-west-1.redshift.amazonaws.com:5439/dev";
-var results = [];
-var graphData = [];
 
+var results = [];
+var graphData = {};
+	
 var client = new pg.Client(conString);
 	client.connect(function(err){
 		if(err)
 			return console.error('could not connect to postgres', err);
-	})
+	});
 
 var port = 3000;
 var server = express();
@@ -21,36 +22,41 @@ server.listen(port, function(){
 	console.log('server listening on port ' + port);
 });
 
-const query = client.query(
-	"select * from(select COUNT(id), primary_type from crimes where primary_type != 'OTHER OFFENSE' group by primary_type ORDER BY COUNT(id) DESC) limit 5");
-
-query.on('row', (row) => {
-	results.push(row);
-});
-
-query.on('end', () => {
-	for(var i = 0; i<results.length; i++){
-		graphData[i] = {
-			"title" : results[i]["primary_type"],
-			"subtitle" : "",
-			"ranges" : [0, results[i]['count']/2, 1300000],
-			"measures" : [parseInt(results[i]['count']), parseInt(results[i]['count'])],
-			"markers" : [parseInt(results[i]['count']) + 100]
-		};
-	}
-});
-
 /*server.post('/first.html', function(req, res){
 	return res.redirect('/first.html');
 });*/
 
-server.post('/update_date', function(req,res){
+var query = client.query("select * from(select COUNT(id), primary_type as column from crimes where primary_type != 'OTHER OFFENSE' group by primary_type ORDER BY COUNT(id) DESC) limit 5");
+query.on('row', (row) => {
+		results.push(row);
+	});
+
+query.on('end', () => {
+	types = [];
+	values = [];
 	
+	for(var i = 0; i<results.length; i++){
+		types[i] = results[i]['column'];
+		values[i] = parseInt(results[i]['count']);
+	}
+	
+	graphData['types'] = types;
+	graphData['values'] = values;
+	console.log(graphData);
+});
+
+server.post('/update_date', function(req,res){
+	if(req.body.display == 1){
+		query = client.query("select * from(select COUNT(id), primary_type as column from crimes where primary_type != 'OTHER OFFENSE' group by primary_type ORDER BY COUNT(id) DESC) limit 5");
+	}
+	else{
+		query = client.query("select COUNT(id), domestic as column from crimes group by domestic order by COUNT(id) DESC");
+		console.log("second query");
+	}
 });
 
 server.get('/get_primary_type', function(req,res){
-	res.setHeader('Content-Type', 'application/json');
 	console.log(graphData);
+	res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify(graphData));
 });
-
